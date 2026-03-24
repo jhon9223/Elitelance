d30a8e3aa931e0e046c599115064e488c65adc0e
John Nj <johnnj2297@gmail.com>
2026-03-24 18:46:26 +0530

    Enable debug to fix login issue

--- elitelance/settings.py
+++ elitelance/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.onrender.com',   # allow render domain
